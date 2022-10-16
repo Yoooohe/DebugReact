bca33c24205398b6a7e9f247c1a44138211ba7a6
yuhe <dingyh94@yahoo.com>
2022-10-16 14:03:26 +0800

    feat: 更新内容

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { React, createRoot } from "./whichReact";
+import "./index.css";
+// import SuspenseListPage from "./pages/SuspenseListPage";
+// import SuspensePage from "./pages/SuspensePage";
+// import UseDeferredValuePage from "./pages/UseDeferredValuePage";
+// import TransitionPage from "./pages/TransitionPage";
+import SetStatePage from "./pages/SetStatePage";
+// import DiffPage from "./pages/DiffPage";
+// import jsx from "./pages/ExamplePage";
+// import App from "./App";
+
+// ReactDOM.render(<SetStatePage />, document.getElementById("root"));
+
+// ReactDOM.render(<DiffPage />, document.getElementById("root"));
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<SetStatePage />);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <UseDeferredValuePage />
+// );
+
+console.log("React", React.version); //sy-log
